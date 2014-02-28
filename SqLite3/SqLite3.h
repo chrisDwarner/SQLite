5b3ce1ce0334b90a2abcfc8ef9e37d29ab94180f
chris warner <chris@BigMac.local>
2014-02-28 15:05:03 -0800

    Initial Commit

---
+++ SqLite3/SqLite3.h
@@ -0,0 +1,13 @@
+//
+//  SqLite3.h
+//  SqLite3
+//
+//  Created by chris warner on 2/28/14.
+//  Copyright (c) 2014 conedogers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SqLite3 : NSObject
+
+@end
---
+++ SqLite3/SqLite3.m
@@ -0,0 +1,13 @@
+//
+//  SqLite3.m
+//  SqLite3
+//
+//  Created by chris warner on 2/28/14.
+//  Copyright (c) 2014 conedogers. All rights reserved.
+//
+
+#import "SqLite3.h"
+
+@implementation SqLite3
+
+@end
